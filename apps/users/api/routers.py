89f7e6264cfaffbdfdd2181d87b9341e499d29c9
GiovanniOlan <giovanniolanperez@gmail.com>
2024-10-28 15:35:28 +0000

    installing simplejwt and making endpoints

--- apps/users/api/routers.py
+++ apps/users/api/routers.py
@@ -1,8 +1,11 @@
-from apps.users.api.views import *
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.users.api.views import *
 
 app_label = "users"
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="refresh_view"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -18,15 +18,14 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # path(
-    #     "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    # ),
+    # drf_yasg(documentation)
     path(
         "docs/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
     path("redocs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("admin/", admin.site.urls),
     path("api/", include("apps.users.api.routers")),
+    # myapps
+    path("admin/", admin.site.urls),
 ]
